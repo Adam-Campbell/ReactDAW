7c7872d6ef449321b2c729d84a6d14ed94626e63
Adam <adamcampbelldev@gmail.com>
2019-04-14 15:12:37 +0100

    Removed unnecessary cloning from audio engine.

--- src/components/AudioEngine/index.js
+++ src/components/AudioEngine/index.js
@@ -1,14 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import * as ActionCreators from '../../actions';
-import Section from './Section';
 import Bus from './Bus';
-import Channel from './Channel';
 import Tone from 'tone';
-import { instrumentTypes, effectTypes } from '../../constants';
-import InstrumentFactory from './InstrumentFactory';
-import EffectFactory from './EffectFactory';
-import { cloneDeep } from 'lodash';
 import { normalizedStateToTree } from './AudioEngineUtils';
 
 window.Tone = Tone;
@@ -37,33 +30,6 @@ class AudioEngine extends Component {
         if (window.bus) {
             delete window.bus;
         }
-    }
-
-    /**
-     * Takes the normalized state from redux and converts it into a tree structure resembling the class
-     * hierarchy within the audio engine. This allows all of the updating that follows to be more intuitive.
-     * @param {object} state - the normalized redux state 
-     * @returns {object} - the state transformed into a tree structure.
-     */
-    _stateToTree(originalState) {
-        const state = cloneDeep(originalState);
-        let tree = {};
-        // copy playerInfo to tree
-        tree.playerInfo = { ...state.playerInfo };
-        // loop over the channels
-        tree.channels = state.channels.map(channel => {
-            return {
-                id: channel.id,
-                instrument: state.instruments[channel.instrumentId],
-                effects: channel.effectIds.map(effectId => state.effects[effectId]),
-                sections: channel.sectionIds.map(sectionId => state.sections[sectionId]),
-                volume: channel.volume,
-                isMuted: channel.isMuted,
-                isSolo: channel.isSolo,
-                pan: channel.pan
-            }
-        });
-        return tree;
     }
 
     /**
